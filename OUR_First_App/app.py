24a00a4c4868daa07f5d1e691eaa9af6a0b95769
martinmwinyi <272393947+martinmwinyi@users.noreply.github.com>
2026-04-22 15:22:15 -0400

    Lab. 385

---
+++ OUR_First_App/app.py
@@ -0,0 +1,27 @@
+# Hashtag means comment (not read by intepreter, used to take notes)
+
+# Primitive Data Types:
+# String - denotated with quotes
+name='Dylan'
+
+# Int - integer ALl WHOLE numbers, positiv or negative
+age=29
+
+# Float - double - decimal point numbers
+dream_hourly_rate=150.75
+
+# Boolean - Bool - True/False - no quotes, must be capitalized
+isAwake=True
+
+# None - DataType - represent null or nothing, no value at all
+money_in_bank=None
+
+# Print statement allows us to PRINT values to the terminal
+print(name)
+print(age)
+print(dream_hourly_rate)
+print(isAwake)
+print(money_in_bank)
+
+# To Run a python application
+# In Terminal Execute (hit enter after): python <nameOfFile>.py
